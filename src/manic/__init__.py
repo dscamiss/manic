9f37ceb69e2ef39f87c2e968a39dba356a383bea
Scott <dscamiss@gmail.com>
2025-02-06 10:19:13 -0800

    Update init files

--- src/manic/__init__.py
+++ src/manic/__init__.py
@@ -1,6 +1,7 @@
-"""Exports for main module."""
+"""/src/manic/__init__.py."""
 
 from src.manic.mechanic import Mechanic, MechanicParams, MechanicState
 from src.manic.mechanic_optimizer import MechanicOptimizer
 
 __all__ = ["MechanicOptimizer", "Mechanic", "MechanicParams", "MechanicState"]
+__doc__ = "A PyTorch implementation of the Mechanic learning rate scheduler."
